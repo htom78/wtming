154070af06498fdd2eee22192ecbb42abf2fec39
htom78 <htom78@gmail.com>
2011-01-28 13:35:29 +0800

    美化页面

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Mrails::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  #TagList.delimiter = " "
+  
 end
 
